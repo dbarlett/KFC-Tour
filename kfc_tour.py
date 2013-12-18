34b83c26095d895d287a4f7ce88dfe2ae66111d0
Dylan Barlett <dylan.barlett@gmail.com>
2013-12-18 16:13:38 -0500

    Expand to all KFC locations in the continental US

--- kfc_tour.py
+++ kfc_tour.py
@@ -4,16 +4,74 @@ import requests
 import simplekml
 from subprocess import call
 
+#  Search points for contintental U.S.
+#  Adapted from http://dev.maxmind.com/geoip/legacy/codes/state_latlon/, plus
+#  supplemental points for large states
+SEARCH_POINTS = [
+    ["AL", -86.8073, 32.799],
+    ["AR", -92.3809, 34.9513],
+    ["AZ", -111.3877, 33.7712],
+    ["CA", -121.8328, 39.7029],  # Chico (Northern CA)
+    ["CA", -119.7798, 36.7465],  # Fresno (Central CA)
+    ["CA", -116.9934, 34.8588],  # Barstow (Southern CA)
+    ["CO", -105.3272, 39.0646],
+    ["CT", -72.7622, 41.5834],
+    #["DC", -77.0262, 38.8964],
+    #["DE", -75.5148, 39.3498],
+    ["FL", -84.2733, 30.4249],  # Tallahassee (North FL)
+    ["FL", -82.4571, 27.9505],  # Tampa (South FL)
+    ["FL", -81.7799, 24.5550],  # Key West (South FL)
+    ["GA", -83.6487, 32.9866],
+    ["IA", -93.214, 42.0046],
+    ["ID", -114.5103, 44.2394],
+    ["IL", -89.0022, 40.3363],
+    ["IN", -86.2604, 39.8647],
+    ["KS", -96.8005, 38.5111],
+    ["KY", -84.6514, 37.669],
+    ["LA", -91.8749, 31.1801],
+    ["MA", -71.5314, 42.2373],
+    ["MD", -76.7902, 39.0724],
+    ["ME", -69.3977, 44.6074],
+    ["MI", -84.5603, 43.3504],
+    ["MN", -93.9196, 45.7326],
+    ["MO", -92.302, 38.4623],
+    ["MS", -89.6812, 32.7673],
+    #["MT", -110.3261, 46.9048],
+    ["NC", -79.8431, 35.6411],
+    ["ND", -99.793, 47.5362],
+    ["NE", -98.2883, 41.1289],
+    ["NH", -71.5653, 43.4108],
+    #["NJ", -74.5089, 40.314],
+    ["NM", -106.2371, 34.8375],
+    ["NV", -117.1219, 38.4199],
+    ["NY", -74.9384, 42.1497],
+    ["OH", -82.7755, 40.3736],
+    ["OK", -96.9247, 35.5376],
+    ["OR", -122.1269, 44.5672],
+    ["PA", -77.264, 40.5773],
+    ["RI", -71.5101, 41.6772],
+    ["SC", -80.9066, 33.8191],
+    ["SD", -99.4632, 44.2853],
+    ["TN", -86.7489, 35.7449],
+    ["TX", -102.3376, 31.7842],  # Odessa (West TX)
+    ["TX", -101.8103, 35.1917],  # Amarillo (North TX)
+    ["TX", -98.4265, 29.4013],   # San Antonio (East TX)
+    ["UT", -111.8535, 40.1135],
+    ["VA", -78.2057, 37.768],
+    ["VT", -72.7093, 44.0407],
+    ["WA", -121.5708, 47.3917],
+    ["WI", -89.6385, 44.2563],
+    ["WV", -80.9696, 38.468],
+    ["WY", -107.2085, 42.7475],
+]
 
-def get_kfc_locations(search_points, state):
-    """Get ID and coordinates for all KFC locations in a state
 
-    :param search_points: iterable of lon, lat tuples to use as starting addresses for search
-    :param state: two-letter state abbreviation
-    :returns: list of KFC locations, each a (lon, lat) tuple
+def get_all_kfc_locations():
+    """Get ID and coordinates for all KFC locations
+    :returns: dict of KFC locations, key = entityID, value = (lon, lat) tuple
     """
 
-    KFC_URL = "http://www.kfc.com/storelocator/Services/SpatialData.svc/findNearbyStoresFiltered"
+    kfc_url = "http://www.kfc.com/storelocator/Services/SpatialData.svc/findNearbyStoresFiltered"
     payload = {
         "dist": 1000,  # Server error if > 1000 (km?)
         "top": 250,  # Server limits responses to 250
@@ -21,15 +79,15 @@ def get_kfc_locations(search_points, state):
 
     locations = {}
 
-    for lon, lat in search_points:
+    for state, lon, lat in SEARCH_POINTS:
         payload["lon"] = lon
         payload["lat"] = lat
-        r = requests.get(KFC_URL, params=payload)
+        r = requests.get(kfc_url, params=payload)
         for i in r.json():
-            if i["state"] == state:
-                locations[i["entityID"]] = (i["longitude"], i["latitude"])
+            locations[i["entityID"]] = (i["longitude"], i["latitude"])
 
-    return locations.values()
+    print "Found %i locations" % len(locations)
+    return locations
 
 
 def write_tsp(filename, nodes):
@@ -49,8 +107,8 @@ def write_tsp(filename, nodes):
         f.write("EOF\n")
 
 
-def read_tsp_solution(filename, nodes):
-    """Read a Concorde TSP solution
+def load_tsp_solution(filename, nodes):
+    """Load a TSP solution
     :param filename: solution file (.sol)
     :param nodes: list of node locations, each a (lon, lat) tuple
     :return: list of node locations in tour order, each a (lon, lat) tuple
@@ -81,24 +139,18 @@ def write_kml(filename, linestring_name, linestring_desc, points):
 
 
 if __name__ == '__main__':
-    state = "CA"
-    ca_points = (
-        (-121.832886, 39.702961),  # Chico (Northern CA)
-        (-119.779816, 36.746587),  # Fresno (Central CA)
-        (-116.993408, 34.858890),  # Barstow (Southern CA)
-    )
+    kfc_us = get_all_kfc_locations()
 
-    kfc_ca = get_kfc_locations(ca_points, "CA")
-    print "Found %d KFC locations in %s" % (len(kfc_ca), state)
-    write_tsp("kfc_ca.tsp", kfc_ca)
+    all_points = kfc_us.values()
+    write_tsp("kfc_us.tsp", all_points)
 
-    call(["./concorde", "kfc_ca.tsp"])
+    call(["./concorde", "kfc_us.tsp"])
 
-    tour = read_tsp_solution("kfc_ca.sol", kfc_ca)
+    tour = load_tsp_solution("kfc_us.sol", all_points)
 
-    write_kml("kfc_ca.kml",
-              "CA KFC Tour",
-              "Tour of California KFC locations",
+    write_kml("kfc_us.kml",
+              "US KFC Tour",
+              "Tour of KFC locations in the Continental U.S.",
               tour
               )
     print "Wrote KML file"
